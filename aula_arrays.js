5b5c684c2ff93d655649b7e3a773d0ae6de6ff43
Davicomaster <davicarneiro.dev@gmail.com>
2026-03-13 19:45:35 -0300

    First Commit

---
+++ aula_arrays.js
@@ -0,0 +1,23 @@
+//Arrays
+//Acessar Elementos
+let alunos = ["josé", "pedro", "mario",]
+console.log(alunos[1]);
+//Saída: pedro
+
+//Modificar Elementos
+let carros = ["Fiat","Peugeot"]
+carros[2] = "Toyota";
+console.log(carros)
+//Saída: ['Fiat', 'Peugeot', 'Toyota']
+
+//Retornar o Numero de Elementos da Lista
+let frutas = ["Banana","Pera","Maçã"];
+console.log(frutas.length);
+//Saída: 3
+
+//Principais Métodos do Array
+// .push(elemento):Adiciona elemento no final do array.
+// .pop():Remove o elemento final do array.
+//.unshift(elemento):Adiciona elemento no inicio do array.
+//.shift():Remove elemento no início do array.
+
